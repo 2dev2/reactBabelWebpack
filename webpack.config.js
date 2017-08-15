189377410f341c2759c1fa7ee37917698f9dab28
dharmadev.gupta <dharmadev.gupta>
2017-08-16 01:13:53 +0530

    eslint setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,26 @@ const path = require("path");
 const APP_DIR = path.resolve(__dirname,"client");
 const BUILD_DIR = path.resolve(__dirname,"dist");
 
+
+//const rules = loaders
 const config = {
 	entry:APP_DIR + "index.js",
 	output :{
 		path:BUILD_DIR,
 		filename:"bundle.js"
+	},
+	module:{
+		loaders:[{
+				test:/\.js$/,
+				exclude:/node_module/,
+				loader:["babel-loader","eslint-loader"]
+			},
+			{
+				test:/\.jsx$/,
+				exclude:/node_module/,
+				loader:"babel-loader"
+			}
+		]
 	}
 };
 
